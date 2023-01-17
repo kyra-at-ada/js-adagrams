d79c5660d8a7aa8a20d6c72d4a0bef7a55dd17b8
maple-megan333 <maple.megan333@gmail.com>
2023-01-17 14:53:34 -0800

    Completed wave 1.

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,43 @@
 export const drawLetters = () => {
   // Implement this method for wave 1
+  let alphabet_pool = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
+  };
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let hand = [];
+  while (hand.length < 10) {
+    const letter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    if (alphabet_pool[letter] > 0) {
+      hand.push(letter);
+      alphabet_pool[letter]--;
+    }
+  }
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
