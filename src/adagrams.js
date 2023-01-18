d865ba2f6fa8a4c841f360d6a39aeebfd78a16a6
maple-megan333 <maple.megan333@gmail.com>
2023-01-17 19:33:40 -0800

    Finished wave 4

--- src/adagrams.js
+++ src/adagrams.js
@@ -60,14 +60,66 @@ export const usesAvailableLetters = (input, lettersInHand) => {
       return false;
     }
   }
-
   return true;
 };
 
+const letter_scores = {
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1,
+  N: 1,
+  R: 1,
+  S: 1,
+  T: 1,
+  D: 2,
+  G: 2,
+  B: 3,
+  C: 3,
+  M: 3,
+  P: 3,
+  F: 4,
+  H: 4,
+  V: 4,
+  W: 4,
+  Y: 4,
+  K: 5,
+  J: 8,
+  X: 8,
+  Q: 10,
+  Z: 10,
+};
+
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const score = word
+    .toUpperCase()
+    .split("")
+    .reduce((accum, letter) => accum + letter_scores[letter], 0);
+  if (score) {
+    return word.length > 6 ? score + 8 : score;
+  }
+  return 0;
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+  const scores = {};
+  for (const word of words) {
+    scores[word] = scoreWord(word);
+  }
+  const max_score = Math.max.apply(Math, Object.values(scores));
+  let winning_words = [];
+  for (const word of words) {
+    if (scores[word] === max_score) {
+      winning_words.push(word);
+    }
+  }
+  winning_words.sort(function (a, b) {
+    return a.length - b.length;
+  });
+  if (winning_words[winning_words.length - 1].length === 10) {
+    winning_words = winning_words.filter((word) => word.length == 10);
+  }
+  return { score: max_score, word: winning_words[0] };
 };
